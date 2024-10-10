49e18fd2817442d46b613ce7484d50a6496d69d0
Yash Duhan <70028651+YashDuhan@users.noreply.github.com>
2024-10-10 13:04:32 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,42 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
 import Home from "./components/Home";
 import VerificationMethods from "./components/VerificationMethods";
 import URLForm from "./Components/VerificationMethod/URLForm";
 import BarcodeScanner from "./Components/VerificationMethod/BarcodeScanner";
 import ManualForm from "./Components/VerificationMethod/ManualForm";
-import MediaPicker from "./Components/VerificationMethod/MediaPicker";  
+import MediaPicker from "./Components/VerificationMethod/MediaPicker";
 import "./App.css";
-import { createBrowserRouter , RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path:"/",
-      element:<Home/>
+      path: "/",
+      element: <Home />,
     },
     {
-      path:"/verification",
-      element:<VerificationMethods/>
+      path: "/verification",
+      element: <VerificationMethods />,
     },
     {
-      path:"/verification/urlform",
-      element:<URLForm />
+      path: "/verification/urlform",
+      element: <URLForm />,
     },
     {
-      path:"/verification/mannualform",
-      element:<ManualForm/>
+      path: "/verification/mannualform",
+      element: <ManualForm />,
     },
     {
-      path:"/verification/media",
-      element:<MediaPicker />
+      path: "/verification/media",
+      element: <MediaPicker />,
     },
     {
-      path:"/verification/barcode",
-      element:<BarcodeScanner/>
-    }
-  ])
+      path: "/verification/barcode",
+      element: <BarcodeScanner />,
+    },
+  ]);
 
   // const [showVerificationMethods, setShowVerificationMethods] = useState(false);
 
--- src/Components/VerificationCard.jsx
+++ src/Components/VerificationCard.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
-import "./VerificationCard.css"
 function VerificationCard({ val }) {
   return (
     <div className="Card">
       <div className="">
         <img src={val.img} className="CardImg"></img>
       </div>
-      <Link to={val.path} className="cardContent btn btn-outline-secondary w-75 mx-auto"> {val.text}</Link>
+      <Link
+        to={val.path}
+        className="cardContent btn btn-outline-secondary w-75 mx-auto">
+        {" "}
+        {val.text}
+      </Link>
     </div>
   );
 }
