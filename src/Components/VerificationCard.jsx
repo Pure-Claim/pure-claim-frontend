b1646c263feb173052f8ada16d79fa1b79d4faa1
Amandeep <145090954+amandeep-boot@users.noreply.github.com>
2024-10-12 22:28:28 +0530

    changes

--- src/Components/VerificationCard.jsx
+++ src/Components/VerificationCard.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
+
 function VerificationCard({ val }) {
   return (
-    <div className="Card ">
-      <div className="">
-        <img src={val.img} className="CardImg"></img>
+    <div className="col-9 col-sm-6 col-md-4 col-lg-3 mb-4 mx-auto ">
+      <div className="card h-100 border  border-success">
+        <img src={val.img} className="img-fluid card-img-top" alt={val.text} />
+        <div className="card-body">
+          <Link
+            to={val.path}
+            className="btn btn-outline-success w-100">
+            {val.text}
+          </Link>
+        </div>
       </div>
-      <Link
-        to={val.path}
-        className="cardContent btn btn-outline-secondary w-75 mx-auto mb-2 ">
-        {" "}
-        {val.text}
-      </Link>
     </div>
   );
 }
+
 export default VerificationCard;
--- src/Components/VerificationMethods.jsx
+++ src/Components/VerificationMethods.jsx
@@ -3,38 +3,39 @@ import Vurl from "../Images/approved.png";
 import Vtext from "../Images/clipboard.png";
 import Vimage from "../Images/camera.png";
 import Vbarcode from "../Images/barcode.jpg";
+import { v4 as uuidv4 } from "uuid";
+
 const VerificationMethods = () => {
   const methods = [
+    { 
+      path: "/verification/urlform", 
+      img: Vurl, 
+      text: "Url Verification" },
     {
-      path:"/verification/urlform",
-      img: Vurl,
-      text: "Url Verification",
-    },
-    {
-      path:"/verification/mannualform",
+      path: "/verification/mannualform",
       img: Vtext,
       text: "Manual Verification",
     },
+    { 
+      path: "/verification/media", 
+      img: Vimage, 
+      text: "Image Verification" },
     {
-      path:"/verification/media",
-      img: Vimage,
-      text: "Image Verification",
-    },
-    {
-      path:"/verification/barcode",
+      path: "/verification/barcode",
       img: Vbarcode,
       text: "Barcode Verification",
     },
   ];
 
   return (
-    <section className="VerificationMethods text-center ">
+    <section className="VerificationMethods text-center">
       <h2 className="Heading">Select Verification Method</h2>
-
-      <div className="cardContainer ">
-        {methods.map((item) => (
-          <VerificationCard key={item} val={item} />
-        ))}
+      <div className="container">
+        <div className="row">
+          {methods.map((item) => (
+            <VerificationCard key={uuidv4()} val={item} />
+          ))}
+        </div>
       </div>
     </section>
   );
