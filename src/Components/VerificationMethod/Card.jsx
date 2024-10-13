eaaec335119e0a14b29132af971b099a0f088824
Amandeep <145090954+amandeep-boot@users.noreply.github.com>
2024-10-13 14:13:14 +0530

    manual form ,API fetch and responsedata Component done

--- src/Components/VerificationMethod/Card.jsx
+++ src/Components/VerificationMethod/Card.jsx
@@ -1,6 +1,6 @@
 function Card({children}){
     return(
-        <div className="w-50  border border-dark border-1 rounded mx-auto p-5 shadow-lg" style={{height:"50vh"}}> 
+        <div className="w-50 my-5 border border-dark border-1 rounded mx-auto p-5 shadow-lg h-auto"> 
         {children}
         </div>
     )
